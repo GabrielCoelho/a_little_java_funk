8c3df46b119ccdfa38eaed9e5ebe566a6a8d3561
GabrielCoelho <gbcoe@outlook.com.br>
2024-12-20 14:47:48 -0300

    fix(Getters & Setters): For all the parameters

--- understanding_oop/bank.java
+++ understanding_oop/bank.java
@@ -7,15 +7,41 @@ public class bank {
   private PessoaFisica owner;
   private double amount;
 
+  // Getters
   public double getAmount() {
     return this.amount;
   }
 
+  public int getAgency() {
+    return this.agency;
+  }
+
+  public int getNumber() {
+    return this.number;
+  }
+
+  public PessoaFisica getOwner() {
+    return this.owner;
+  }
+
+  // Setters
   public void setAmount(double value) {
     if (value < 0) {
       return;
     }
     this.amount = value;
+  }
+
+  public void setAgency(int value) {
+    this.agency = value;
+  }
+
+  public void setNumber(int value) {
+    this.number = value;
+  }
+
+  public void setOwner(PessoaFisica newPessoa) {
+    this.owner = newPessoa;
   }
 
   public void deposit(double newDeposit) {
