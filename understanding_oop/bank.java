218b867b3b95c789148b3127196b21f60efe589a
GabrielCoelho <gbcoe@outlook.com.br>
2024-12-20 14:42:34 -0300

    feat(Encapsulation): Getters and Setters

--- understanding_oop/bank.java
+++ understanding_oop/bank.java
@@ -2,16 +2,27 @@
  * bank
  */
 public class bank {
-  int agency;
-  int number;
-  PessoaFisica owner;
-  double amount;
+  private int agency;
+  private int number;
+  private PessoaFisica owner;
+  private double amount;
 
-  void deposit(double newDeposit) {
+  public double getAmount() {
+    return this.amount;
+  }
+
+  public void setAmount(double value) {
+    if (value < 0) {
+      return;
+    }
+    this.amount = value;
+  }
+
+  public void deposit(double newDeposit) {
     this.amount += newDeposit;
   }
 
-  double withdraw(double value) {
+  public double withdraw(double value) {
     if (this.amount < value) {
       System.out.println("Insuficient founds");
       return 0;
@@ -21,7 +32,7 @@ public class bank {
 
   }
 
-  boolean transfer(double value, bank destinyAccount) {
+  public boolean transfer(double value, bank destinyAccount) {
     double v = this.withdraw(value);
     if (v != 0) {
       destinyAccount.amount += value;
@@ -33,35 +44,11 @@ public class bank {
 
   public static void main(String[] args) {
     bank account = new bank();
-    PessoaFisica p1 = new PessoaFisica();
 
-    p1.name = "Gabriel";
-    p1.cpf = "12345678900";
-    p1.surname = "Coelho Soares";
-    p1.jobTitle = "Developer";
-    p1.monthlySalary = 2500;
-
-    account.agency = 112;
-    account.number = 1287634;
-    account.owner = p1;
-    account.amount = 8427.387;
-
-    System.out.println("Owner: " + account.owner.name + " \nAgency number: " + account.number + " \tAccount Number: "
-        + account.number + "\nAmount stored: R$ " + account.amount);
-
-    account.deposit(1500);
-    System.out.println("Owner: " + account.owner.name + " \nAgency number: " + account.number + " \tAccount Number: "
-        + account.number + "\nAmount stored: R$ " + account.amount);
-
-    double s = account.withdraw(300);
-    System.out.println(s);
-
-    s = account.withdraw(2198347);
-
-    bank newTest = new bank();
-
-    account.transfer(3000, newTest);
-
-    System.out.println(newTest.amount);
+    account.deposit(100);
+    account.withdraw(1500);
+    System.out.println(account.getAmount());
+    account.setAmount(-1500);
+    System.out.println(account.getAmount());
   }
 }
