c4a3150ad929b013865dc44c69330e287085716b
GabrielCoelho <gbcoe@outlook.com.br>
2024-12-20 12:15:01 -0300

    feat(variables): Primitive variables

---
+++ variables/variables.java
@@ -0,0 +1,33 @@
+public class variables {
+  public static void main(String[] args) {
+    // reviewing variables declaration
+
+    int age;
+    age = 29;
+
+    short _second_age;
+    _second_age = 30;
+
+    long _thirdAge;
+    _thirdAge = 31;
+
+    System.out.println("I'm " + age + " years old, next year I'll be " + _second_age);
+
+    byte top = 127;
+
+    float nota = 9;
+    double media = 8.12987346;
+
+    boolean estaEhVerdade = false;
+    boolean aOutraEstaMentindo = true;
+
+    char letter = 'a';
+    String phrase = new String();
+
+    // Casting
+
+    double salary = 1412.89;
+    int salaryValue = (int) salary;
+
+  }
+}
